88e0444016c9fc2ed9df9fbc98fd7072df278a36
Amadeus-Winarto <amadeusw6@gmail.com>
2020-11-29 22:11:42 +0800

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
+const return_value =  <div className="App">
+                        <Navbar dark color="primary">
+                          <div className="container">
+                            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                          </div>
+                        </Navbar>
+                      </div>;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return return_value;
 }
 
 export default App;
