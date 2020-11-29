3fe90b69632e01638964d65508f2c937a5239c24
Amadeus-Winarto <amadeusw6@gmail.com>
2020-11-29 22:34:03 +0800

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 // import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-const return_value =  <div className="App">
+function App() {
+  const return_value =  <div className="App">
                         <Navbar dark color="primary">
                           <div className="container">
                             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                           </div>
                         </Navbar>
+                        <Menu />
                       </div>;
-function App() {
   return return_value;
 }
 
